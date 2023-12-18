14a88a61c2c5a62bbb29370c4a0cf16cab20c53f
Ernst Pluess <ernst@pluess.li>
2023-12-18 14:38:59 +0100

    initial webserver

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
 #include "credentials.h"
+#include <ESPAsyncWebServer.h>
+
+AsyncWebServer server(80);
+
+const char* PARAM_MESSAGE = "message";
 
 void initWiFi() {
   WiFi.mode(WIFI_STA);
@@ -16,9 +23,30 @@ void initWiFi() {
 void setup() {
   Serial.begin(9600);
   initWiFi();
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("/");
+    request->send(200, "text/plain", "Hello, world");
+  });
+
+      // Send a GET request to <IP>/get?message=<message>
+    server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+        String message;
+        if (request->hasParam(PARAM_MESSAGE)) {
+            message = request->getParam(PARAM_MESSAGE)->value();
+        } else {
+            message = "No message sent";
+        }
+        Serial.println("/get");
+        request->send(200, "text/plain", "Hello, GET: " + message);
+    });
+
+
+  server.begin();
+
 }
 
 void loop() {
-  Serial.println("Hello world!");
-  delay(1000);
+    Serial.print('=');
+    delay(2000);
 }
