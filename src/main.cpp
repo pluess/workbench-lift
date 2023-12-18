f690dacbee749b4ed7353e56c5b3c0a9e3ad163f
Ernst Pluess <ernst@pluess.li>
2023-12-18 15:39:50 +0100

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,44 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <WiFi.h>
+#include <SPIFFS.h>
 #include <AsyncTCP.h>
 #include "credentials.h"
 #include <ESPAsyncWebServer.h>
 
 AsyncWebServer server(80);
 
-const char* PARAM_MESSAGE = "message";
+const char *PARAM_MESSAGE = "message";
 
-void initWiFi() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(1000);
-  }
-  Serial.println(WiFi.localIP());
+void initWiFi()
+{
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to WiFi ..");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print('.');
+        delay(1000);
+    }
+    Serial.println(WiFi.localIP());
 }
 
-void setup() {
-  Serial.begin(9600);
-  initWiFi();
+void setup()
+{
+    SPIFFS.begin();
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.begin(9600);
+    initWiFi();
+
+    server.serveStatic("/index.html", SPIFFS, "/index.html");
+
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
     Serial.println("/");
-    request->send(200, "text/plain", "Hello, world");
-  });
+    request->send(200, "text/plain", "Hello, world"); });
 
-      // Send a GET request to <IP>/get?message=<message>
-    server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    // Send a GET request to <IP>/get?message=<message>
+    server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
         String message;
         if (request->hasParam(PARAM_MESSAGE)) {
             message = request->getParam(PARAM_MESSAGE)->value();
@@ -38,15 +46,13 @@ void setup() {
             message = "No message sent";
         }
         Serial.println("/get");
-        request->send(200, "text/plain", "Hello, GET: " + message);
-    });
+        request->send(200, "text/plain", "Hello, GET: " + message); });
 
-
-  server.begin();
-
+    server.begin();
 }
 
-void loop() {
+void loop()
+{
     Serial.print('=');
     delay(2000);
 }
