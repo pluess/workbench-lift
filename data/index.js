fccaf690928849d14cba687708ab956ed75a632e
Ernst Pluess <ernst@pluess.li>
2023-12-21 14:51:44 +0100

    updateAll for sliders, radio buttions

--- data/index.js
+++ data/index.js
@@ -1,3 +1,12 @@
+function upateAll() {
+    var sliderValue = $("#allpwm").val();
+    $("text_allpwm").html(sliderValue);
+    console.log("sliderValue=" + sliderValue);
+    for (var i = 1; i <= 4; i++) {
+        $("#motor" + i).val(sliderValue).trigger("change");
+    }
+}
+
 function updateSliderPWM(element) {
     var sliderValue = $(element).val();
     $("#text_" + element.id).html(sliderValue);
@@ -16,9 +25,6 @@ function updateSliderPWM(element) {
             type: "GET"
         }
     )
-    // var xhr = new XMLHttpRequest();
-    // xhr.open("GET", "/set-pwm-" + element.id + "?pwm=" + sliderValue + "&mode=" + mode, true);
-    // xhr.send();
 }
 
 function resetFunction() {
@@ -27,3 +33,36 @@ function resetFunction() {
         $("#motor" + i).val(50).trigger("change");
     }
 }
+
+function a(event) {
+    console.log("got it");
+    switch ($(this).val()) {
+        case 'bwAll':
+            console.log("bwAll");
+            break;
+        case 'fwAll':
+            console.log("fwAll");
+            break;
+        case 'offAll':
+            console.log("offAll");
+            break;
+    }
+}
+
+$(document).ready(function () {
+    $('input[type=radio][name=driveAll]').on('change', function () {
+        switch ($(this).val()) {
+            case 'bwAll':
+                console.log("bwAll");
+                break;
+            case 'fwAll':
+                console.log("fwAll");
+                break;
+            case 'offAll':
+                console.log("offAll");
+                break;
+        }
+    });
+    $('input[type=radio][name=drive1]').on('change', a);
+});
+
